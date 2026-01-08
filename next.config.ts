f547e391fffe670591d827f588ef960b7645d02f
AjithkumarAnandan <ajithkumaranandandeveloper@gmail.com>
2026-01-08 15:14:05 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/ecommerce',
+  assetPrefix: '/ecommerce',  
   productionBrowserSourceMaps: false,
   experimental: {
     serverActions: {
