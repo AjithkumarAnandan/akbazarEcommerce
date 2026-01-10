f91548dff5a5622bbcb5424fe6202b82faf46cf9
AjithkumarAnandan <ajithkumaranandandeveloper@gmail.com>
2026-01-10 08:46:26 +0530

    remove baseurl

--- utils/api.path.ts
+++ utils/api.path.ts
@@ -1,3 +1,31 @@
+// //Front end navigation
+// export const defaultPath="/";
+// export const signupPath="/signup";
+// export const contactPath="/contact";
+// export const logoutPath="/logout";
+// export const loginPath="/login";
+// export const dashboardClientPath="/dashboard";
+// export const dashboardPath="/ecommerce/dashboard";
+
+// //Backend navigation
+// export const authLogin="/ecommerce/api/auth/login"
+// export const authLogout="/ecommerce/api/auth/logout"
+// export const authSignupApi="/ecommerce/api/auth/signup"
+// export const authRefreshApi="/ecommerce/api/auth/refresh"
+// export const dashboardApi="/ecommerce/api/dashboard"
+// export const productIdApi="/ecommerce/api/selected-product"
+// export const addProductApi="/ecommerce/api/addproduct";
+
+
+// //Images
+// export const svgSearchImage="/ecommerce/search.svg";
+// export const wishlistImage="/ecommerce/wishlist.svg";
+// export const wishlistFavImage="/ecommerce/wishlist-favorite.svg";
+// export const frameImage="/ecommerce/Frame.svg";
+// export const cartImage="/ecommerce/cart.svg";
+// export const phoneIconImage="/ecommerce/icons-phone.svg";
+// export const mailIconImage="/ecommerce/icons-mail.svg";
+
 //Front end navigation
 export const defaultPath="/";
 export const signupPath="/signup";
@@ -5,23 +33,23 @@ export const contactPath="/contact";
 export const logoutPath="/logout";
 export const loginPath="/login";
 export const dashboardClientPath="/dashboard";
-export const dashboardPath="/ecommerce/dashboard";
+export const dashboardPath="/dashboard";
 
 //Backend navigation
-export const authLogin="/ecommerce/api/auth/login"
-export const authLogout="/ecommerce/api/auth/logout"
-export const authSignupApi="/ecommerce/api/auth/signup"
-export const authRefreshApi="/ecommerce/api/auth/refresh"
-export const dashboardApi="/ecommerce/api/dashboard"
-export const productIdApi="/ecommerce/api/selected-product"
-export const addProductApi="/ecommerce/api/addproduct";
+export const authLogin="/api/auth/login"
+export const authLogout="/api/auth/logout"
+export const authSignupApi="/api/auth/signup"
+export const authRefreshApi="/api/auth/refresh"
+export const dashboardApi="/api/dashboard"
+export const productIdApi="/api/selected-product"
+export const addProductApi="/api/addproduct";
 
 
 //Images
-export const svgSearchImage="/ecommerce/search.svg";
-export const wishlistImage="/ecommerce/wishlist.svg";
-export const wishlistFavImage="/ecommerce/wishlist-favorite.svg";
-export const frameImage="/ecommerce/Frame.svg";
-export const cartImage="/ecommerce/cart.svg";
-export const phoneIconImage="/ecommerce/icons-phone.svg";
-export const mailIconImage="/ecommerce/icons-mail.svg";
+export const svgSearchImage="/search.svg";
+export const wishlistImage="/wishlist.svg";
+export const wishlistFavImage="/wishlist-favorite.svg";
+export const frameImage="/Frame.svg";
+export const cartImage="/cart.svg";
+export const phoneIconImage="/icons-phone.svg";
+export const mailIconImage="/icons-mail.svg";
